1bd71b160a5f7e91d7b456abf150a9ac4dc6fe69
Moldir <moldir.andirbekova@mail.ru>
2022-12-20 00:59:21 +0600

    vse

--- button-drink/drink.js
+++ button-drink/drink.js
@@ -1,9 +1,23 @@
 function box(){
     let a = document.getElementById("number").innerText
-    swal({
-        title: "You have added " + document.getElementById("number").innerText + " " + "drink",
-        text: "",
-        icon: 'success',
-        button: 'Close'
-    });
+    Swal.fire({
+        title: 'Thank you!',
+        text: "You have added " + document.getElementById("number").innerText + " " + 'drinks',
+        type: 'success',
+        showCancelButton: true,
+        confirmButtonColor: 'LightSeaGreen',
+        cancelButtonColor: 'Crimson',
+        confirmButtonText: 'Okay!'
+      }).then((result) => {
+        if (result.value) {
+          Swal.fire({
+            type: 'success',
+            title: 'Thank You!',
+            text: "The message to confirm is sent to your email!",
+            timer: 2000,
+            showCancelButton: false,
+            showConfirmButton: false
+          })
+        }
+      })
 }
